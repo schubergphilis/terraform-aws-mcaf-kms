b05e02f6f0db637e49c45468c6201905adcb1647
Sander van Harmelen <sander@vanharmelen.nl>
2019-08-02 21:27:55 +0200

    The quotes aren't needed, but lots of tools expect them

--- variables.tf
+++ variables.tf
@@ -1,32 +1,32 @@
-variable name {
+variable "name" {
   type        = string
   description = "The name of the key"
 }
 
-variable description {
+variable "description" {
   type        = string
   description = "The description of the key as viewed in AWS console"
 }
 
-variable policy {
+variable "policy" {
   type        = string
   default     = ""
   description = "A valid KMS policy JSON document"
 }
 
-variable deletion_window_in_days {
+variable "deletion_window_in_days" {
   type        = number
   default     = 30
   description = "Delay in days after which the key is deleted"
 }
 
-variable enable_key_rotation {
+variable "enable_key_rotation" {
   type        = bool
   default     = false
   description = "Specifies whether key rotation is enabled"
 }
 
-variable tags {
+variable "tags" {
   type        = map(string)
   description = "A mapping of tags to assign to the key"
 }
