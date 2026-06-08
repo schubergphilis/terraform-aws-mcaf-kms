574e5f53ee1ea37ff4a689951a072d4cb7a8c26c
Marwin Baumann <mbaumann@schubergphilis.com>
2026-06-08 13:11:22 +0200

    feat: add missing variables of aws_kms_key

--- main.tf
+++ main.tf
@@ -5,11 +5,16 @@ resource "aws_kms_alias" "default" {
 }
 
 resource "aws_kms_key" "default" {
-  region                  = var.region
-  deletion_window_in_days = var.deletion_window_in_days
-  description             = coalesce(var.description, var.name)
-  enable_key_rotation     = var.enable_key_rotation
-  is_enabled              = true
-  policy                  = var.policy
-  tags                    = var.tags
+  region                   = var.region
+  custom_key_store_id      = var.custom_key_store_id
+  customer_master_key_spec = var.customer_master_key_spec
+  deletion_window_in_days  = var.deletion_window_in_days
+  description              = coalesce(var.description, var.name)
+  enable_key_rotation      = var.enable_key_rotation
+  is_enabled               = true
+  key_usage                = var.key_usage
+  multi_region             = var.multi_region
+  policy                   = var.policy
+  xks_key_id               = var.xks_key_id
+  tags                     = var.tags
 }
